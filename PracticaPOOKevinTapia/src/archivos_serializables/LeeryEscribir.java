32f91a0e29231da2f2eae6c335133679854aecff
its <its@ISTLOJA02>
2019-08-16 09:44:09 +1200

    16/08/2019 trabajo en clase "listas en serializables"

--- PracticaPOOKevinTapia/src/archivos_serializables/LeeryEscribir.java
+++ PracticaPOOKevinTapia/src/archivos_serializables/LeeryEscribir.java
@@ -39,7 +39,37 @@ public class LeeryEscribir {
             System.out.println(e);
         }
     }
-    
+    public void leerLista(String url){//lee toda la lista
+        try {
+             ObjectInputStream lector =new ObjectInputStream(new FileInputStream(url));
+             Object auxiliar =lector.readObject();
+              while(auxiliar!=null){
+               Persona p1 =(Persona)auxiliar;
+                  System.out.println("Dueño: " + p1.getNombre() + " " + "Nombre de la mascota: " + " " + p1.getMascota().getNombre() + " " + "Su edad es: " + p1.getMascota().getEdad());
+              auxiliar=lector.readObject();
+              }
+        } catch (Exception e) {
+            System.out.println("error"+e);
+        }
+ 
+    }
+    public List<Persona> leerPersonas(String url){
+   
+        List<Persona> Lista = new ArrayList<>();
+             try {
+             ObjectInputStream lector =new ObjectInputStream(new FileInputStream(url));
+             Object auxiliar =lector.readObject();
+              while(auxiliar!=null){
+               Persona p1 =(Persona)auxiliar;
+                Lista.add(p1);
+              auxiliar=lector.readObject();
+              }
+        } catch (Exception e) {
+            System.out.println("error"+e);
+        }
+        return Lista ;  
+             
+       }
     public void escribirLista(String direccion, List<Persona> lista) {
         try {
             ObjectOutputStream escritorio = new ObjectOutputStream(new FileOutputStream(direccion));
@@ -62,14 +92,25 @@ public class LeeryEscribir {
         Persona p6 = new Persona("jhoel", "kawai", 982369, 55, new Mascota("vicha1", 3, 4));
         List<Persona> Lista = new ArrayList<>();//creamos la lista para guarar objetos
         //una vez que creasmos la lista ponemos los objetos dentro de ella
+        
         Lista.add(p2);
         Lista.add(p4);
         Lista.add(p3);
         Lista.add(p5);
         Lista.add(p6);
-        objeto.escribirLista("C:\\\\Users\\\\its\\\\Desktop\\\\ejemplo.txt", Lista);
-
+        int a = Lista.size();//guardo en la variable a esta me permite saber el tamaño de lista (.size())
+        System.out.println(a);//obtengo el tamaño de la lista
+//        for (Persona perso : Lista) {
+//            System.out.println(perso.getNombre()); 
+//        }
+        
+         //objeto.escribirLista("C:\\\\Users\\\\its\\\\Desktop\\\\ejemplo.txt", Lista);
         // objeto.escribirObjeto("C:\\Users\\its\\Desktop\\ejemplo.txt", p2);
         //objeto.leerArchivo("C:\\\\Users\\\\its\\\\Desktop\\\\ejemplo.txt");
+        //objeto.leerLista("C:\\\\Users\\\\its\\\\Desktop\\\\ejemplo.txt");
+        List<Persona> b=objeto.leerPersonas("C:\\\\Users\\\\its\\\\Desktop\\\\ejemplo.txt");
+        for (Persona persona : b) {
+            System.out.println(persona.getEdad());
+        }
     }
 }
